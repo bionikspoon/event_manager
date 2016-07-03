9c5bb220d0d42df0453ebdd848e59fd15f5a6e39
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-03 00:24:38 -0500

    use sunlight to get legislators

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,7 @@
 require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = File.read('./.env.API_KEY').chomp
 
 
 def main
@@ -8,7 +11,8 @@ def main
   contents.each do |row|
     name = row[:first_name]
     zipcode = clean_zipcode row[:zipcode]
-    puts "#{name} #{zipcode}"
+    legislators = legislators_by_zipcode zipcode
+    puts "#{name} #{zipcode} #{legislators}"
   end
 end
 
@@ -16,4 +20,13 @@ def clean_zipcode zipcode
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-main
+def legislators_by_zipcode zipcode
+  Sunlight::Congress::Legislator
+      .by_zipcode(zipcode)
+      .collect { |legislator| "#{legislator.first_name} #{legislator.last_name}" }
+      .join(', ')
+end
+
+if __FILE__ == $0
+  main
+end
