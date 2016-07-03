ef74523f98b5c4e82d40ebc3f3a41107b623f42c
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-02 21:15:19 -0500

    read data

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,9 @@
 puts "Event Manager Initialized!"
+
+lines = File.readlines 'event_attendees.csv'
+lines.each_with_index do |line, i|
+  next if i == 0
+  columns = line.split(',')
+  names = columns[2]
+  puts names
+end
